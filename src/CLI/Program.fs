f10c7a30caa884575a01cd5348480f8fbd68bf1f
Jordan Cannon <LiteracyFanatic@gmail.com>
2022-05-19 07:24:54 -0500

    Add version info to CLI based on git

--- src/CLI/Program.fs
+++ src/CLI/Program.fs
@@ -1,4 +1,6 @@
 open System.Text
+open System.Reflection
+open System.Linq
 open Microsoft.Data.Sqlite
 open Kensaku
 open Kensaku.Core.Kanji
@@ -121,10 +123,12 @@ let kanjiHandler (args: ParseResults<KanjiArgs>) =
 
 type Args =
     | [<CliPrefix(CliPrefix.None)>] Kanji of ParseResults<KanjiArgs>
+    | Version
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | Kanji _ -> "search for kanji"
+            | Version -> "display the version info"
 
 [<EntryPoint>]
 let main argv =
@@ -137,7 +141,18 @@ let main argv =
             errorHandler = ProcessExiter(),
             usageStringCharacterWidth = 80)
     let results = parser.ParseCommandLine(argv)
-    match results.GetSubCommand() with
-    | Kanji kanjiArgs -> kanjiHandler kanjiArgs
+    if results.Contains(Version) then
+        let version =
+            Assembly.GetEntryAssembly()
+                .GetCustomAttributes<AssemblyMetadataAttribute>()
+                .First(fun a -> a.Key = "GitTag")
+                .Value
+        printfn "%s" version
+    else
+        match results.GetSubCommand() with
+        | Kanji kanjiArgs -> kanjiHandler kanjiArgs
+        | Version ->
+            let versionOptionName = results.Parser.GetArgumentCaseInfo(Version).Name.Value
+            results.Raise($"%s{versionOptionName} should be handled before evaluation of subcommands")
 
     0
