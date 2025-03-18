2d8a401e32fe95dc4de84920aa23228433f32f53
Jordan Cannon <LiteracyFanatic@gmail.com>
2025-03-18 12:28:56 -0500

    Fix RuneJsonConverter.Read

--- src/CLI/RuneJsonConverter.fs
+++ src/CLI/RuneJsonConverter.fs
@@ -4,17 +4,15 @@ open System
 open System.Text
 open System.Text.Json
 open System.Text.Json.Serialization
+open Kensaku
 
 type RuneJsonConverter() =
     inherit JsonConverter<Rune>()
 
     override this.Read(reader: byref<Utf8JsonReader>, typeToConvert: Type, options: JsonSerializerOptions) =
-        let value = reader.GetString()
-
-        if value.Length = 1 then
-            Rune(value[0])
-        else
-            raise (JsonException("Expected a single character"))
+        match reader.GetString() |> String.getRunes with
+        | [ rune ] -> rune
+        | _ -> raise (JsonException("Expected a single character"))
 
     override this.Write(writer: Utf8JsonWriter, value: Rune, options: JsonSerializerOptions) =
         writer.WriteStringValue(value.ToString())
