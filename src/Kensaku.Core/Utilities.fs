2046e89294a95189e1b20229cc8302ada2b6a086
Jordan Cannon <LiteracyFanatic@gmail.com>
2025-03-29 02:06:28 -0500

    Remove Japanese text detection helpers from Kensaku.Core

--- src/Kensaku.Core/Utilities.fs
+++ src/Kensaku.Core/Utilities.fs
@@ -9,18 +9,4 @@ module Utilities =
     module String =
         let getRunes (input: string) = [ for rune in input.EnumerateRunes() -> rune ]
 
-    module Char =
-        let isHiragana (c: char) = c >= '\u3040' && c <= '\u309f'
-
-        let isKatakana (c: char) = c >= '\u30a0' && c <= '\u30ff'
-
-        let isKana (c: char) = isHiragana c || isKatakana c
-
-        let isKanji (c: char) =
-            (c >= '\u4e00' && c <= '\u9fcf')
-            || (c >= '\uf900' && c <= '\ufaff')
-            || (c >= '\u3400' && c <= '\u4dbf')
-
-        let isJapanese (c: char) = isKana c || isKanji c
-
     let inline rune input = Rune.GetRuneAt(string input, 0)
