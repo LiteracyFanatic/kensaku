03c567a07697a6e49b3e5ea3a2c8a9f27711012d
Jordan Cannon <LiteracyFanatic@gmail.com>
2025-10-09 19:44:30 -0500

    Add radical subcommand
    Supports search by radical number, name, meaning, exact stroke count, stroke
    range, or literal value

---
+++ src/Kensaku.CLI/RadicalCommand.fs
@@ -0,0 +1,157 @@
+namespace Kensaku.CLI
+
+
+module RadicalCommand =
+    open Argu
+    open Spectre.Console
+
+    open Kensaku.CLI.Formatting
+    open Kensaku.Core
+    open Kensaku.Core.Radicals
+
+    type RadicalArgs =
+        | Strokes of int
+        | Min_Strokes of int
+        | Max_Strokes of int
+        | Number of int
+        | Name of string
+        | Meaning of string
+        | [<MainCommand; Last>] Radical of string list
+        | Format of Format
+        | No_Pager
+
+        interface IArgParserTemplate with
+            member this.Usage =
+                match this with
+                | Strokes _ -> "search for a radical with the given number of strokes"
+                | Min_Strokes _ -> "search for radicals with at least the given number of strokes"
+                | Max_Strokes _ -> "search for radicals with at most the given number of strokes"
+                | Number _ -> "search for a radical by its number"
+                | Name _ -> "search for a radical by its name"
+                | Meaning _ -> "search for a radical by its meaning (Unicode or WaniKani)"
+                | Radical _ -> "show info for the given radical character(s)"
+                | Format _ -> "output format"
+                | No_Pager -> "do not use a pager"
+
+    let validateStrokeArgs (args: ParseResults<RadicalArgs>) =
+        if
+            args.Contains(Strokes)
+            && (args.Contains(Min_Strokes) || args.Contains(Max_Strokes))
+        then
+            let strokeArgName = args.Parser.GetArgumentCaseInfo(Strokes).Name.Value
+
+            let minMaxArgNames =
+                [
+                    args.Parser.GetArgumentCaseInfo(Min_Strokes).Name.Value
+                    args.Parser.GetArgumentCaseInfo(Max_Strokes).Name.Value
+                ]
+                |> String.concat " or "
+
+            args.Raise($"%s{strokeArgName} can not be used with %s{minMaxArgNames}")
+
+    let isSearchOption (arg: RadicalArgs) =
+        match arg with
+        | Strokes _
+        | Number _
+        | Name _
+        | Meaning _
+        | Radical _ -> true
+        | Min_Strokes _
+        | Max_Strokes _
+        | Format _
+        | No_Pager -> false
+
+    let getSearchOptions (args: ParseResults<RadicalArgs>) =
+        args.GetAllResults() |> List.filter isSearchOption
+
+    let hasStrokeRangeOption (args: ParseResults<RadicalArgs>) =
+        args.Contains(Min_Strokes) || args.Contains(Max_Strokes)
+
+    let validateAtLeastOneArg (args: ParseResults<RadicalArgs>) =
+        let searchOptions = getSearchOptions args
+
+        let hasAny =
+            searchOptions.Length > 0 || args.Contains(Strokes) || hasStrokeRangeOption args
+
+        if not hasAny then
+            args.Raise("You must specify at least one search option")
+
+    let validateNoOtherSearchOptionsWithLiteralRadical (args: ParseResults<RadicalArgs>) =
+        let searchOptions = getSearchOptions args
+        let hasOtherStrokeOptions = args.Contains(Strokes) || hasStrokeRangeOption args
+
+        if args.Contains(Radical) && (searchOptions.Length > 1 || hasOtherStrokeOptions) then
+            args.Raise("You can not use other search options when passing a literal radical")
+
+    let validateOnlyOneSearchOption (args: ParseResults<RadicalArgs>) =
+        if not (args.Contains(Radical)) then
+            let searchCount =
+                (if args.Contains(Number) then 1 else 0)
+                + (if args.Contains(Name) then 1 else 0)
+                + (if args.Contains(Meaning) then 1 else 0)
+                + (if args.Contains(Strokes) then 1 else 0)
+                + (if hasStrokeRangeOption args then 1 else 0)
+
+            if searchCount > 1 then
+                args.Raise("You can only use one search option at a time")
+
+    let validateRadicalArgs (args: ParseResults<RadicalArgs>) =
+        validateStrokeArgs args
+        validateAtLeastOneArg args
+        validateNoOtherSearchOptionsWithLiteralRadical args
+        validateOnlyOneSearchOption args
+
+    let radicalHandler (ctx: KensakuConnection) (args: ParseResults<RadicalArgs>) =
+        validateRadicalArgs args
+
+        let radicals =
+            match args.TryGetResult(Radical) with
+            | Some radicals ->
+                let radicalRunes =
+                    radicals |> List.collect (fun s -> s.EnumerateRunes() |> Seq.toList)
+
+                getRadicalLiteralsAsync radicalRunes ctx
+                |> Async.AwaitTask
+                |> Async.RunSynchronously
+                |> Seq.toList
+            | None ->
+                let minStrokeCount, maxStrokeCount =
+                    match args.TryGetResult Strokes with
+                    | Some n -> Some n, Some n
+                    | None -> args.TryGetResult Min_Strokes, args.TryGetResult Max_Strokes
+
+                let query = {
+                    RadicalNumber = args.TryGetResult Number
+                    RadicalName = args.TryGetResult Name
+                    RadicalMeaning = args.TryGetResult Meaning
+                    MinStrokeCount = minStrokeCount
+                    MaxStrokeCount = maxStrokeCount
+                }
+
+                getRadicalsAsync query ctx
+                |> Async.AwaitTask
+                |> Async.RunSynchronously
+                |> Seq.toList
+
+        match radicals with
+        | [] -> ()
+        | _ ->
+            match args.TryGetResult(Format) |> Option.defaultValue Format.Text with
+            | Format.Text ->
+                let console = StringWriterAnsiConsole()
+
+                for i in 0 .. radicals.Length - 1 do
+                    printRadical console radicals[i]
+
+                    if i < radicals.Length - 1 then
+                        console.WriteLine()
+                        (console :> IAnsiConsole).Write(Rule())
+                        console.WriteLine()
+
+                let text = console.ToString()
+
+                if args.Contains(No_Pager) then
+                    printf "%s" text
+                else
+                    toPager text
+            | Format.Json -> radicals |> toJson |> printfn "%s"
---
+++ src/Kensaku.Core/Radicals.fs
@@ -0,0 +1,174 @@
+namespace Kensaku.Core
+
+module Radicals =
+    open System.Text
+
+    open Dapper
+
+    type GetRadicalQuery = {
+        RadicalNumber: int option
+        RadicalName: string option
+        RadicalMeaning: string option
+        MinStrokeCount: int option
+        MaxStrokeCount: int option
+    }
+
+    type RadicalQueryResult = {
+        Id: int
+        Number: int option
+        StrokeCount: int
+        Values: Rune list
+        Meanings: string list
+        Names: string list
+        KanjiCount: int
+    }
+
+    [<CLIMutable>]
+    type private RadicalKanjiCount = {
+        RadicalId: int
+        Count: int
+    }
+
+    [<CLIMutable>]
+    type private RadicalNameValue = {
+        RadicalId: int
+        Value: string
+    }
+
+    let private getRadicalIdsAsync (query: GetRadicalQuery) (ctx: KensakuConnection) =
+        task {
+            return!
+                ctx.QueryAsync<int>(
+                    sql
+                        """
+                    select distinct r.Id
+                    from Radicals as r
+                    where true
+                    and (@RadicalNumber is null or r.Number = @RadicalNumber)
+                    and (
+                        @RadicalName is null or exists (
+                            select 1
+                            from RadicalValues as rv
+                            join Characters as c on c.Value = rv.Value and c.IsRadical = 1
+                            join RadicalNames as rn on rn.CharacterId = c.Id
+                            where rv.RadicalId = r.Id and rn.Value = @RadicalName
+                        )
+                    )
+                    and (
+                        @RadicalMeaning is null or (
+                            exists (
+                                select 1 from RadicalMeanings as rm
+                                where rm.RadicalId = r.Id and rm.Value like @RadicalMeaning
+                            )
+                            or exists (
+                                select 1 from RadicalValues as rv2
+                                where rv2.RadicalId = r.Id and rv2.Value = @RadicalMeaning
+                            )
+                        )
+                    )
+                    and (@MinStrokeCount is null or r.StrokeCount >= @MinStrokeCount)
+                    and (@MaxStrokeCount is null or r.StrokeCount <= @MaxStrokeCount)
+                    order by r.Number, r.StrokeCount
+                    """,
+                    param = query
+                )
+        }
+
+    let private getRadicalsByIdsAsync (ids: int seq) (ctx: KensakuConnection) =
+        task {
+            if Seq.isEmpty ids then
+                return Seq.empty
+            else
+                let param = {| Ids = ids |}
+
+                let! radicals = ctx.QueryAsync<Tables.Radical>(sql "select * from Radicals where Id in @Ids", param)
+                let radicalsById = radicals |> Seq.map (fun x -> x.Id, x) |> Map.ofSeq
+
+                let! radicalValues =
+                    ctx.QueryAsync<Tables.RadicalValue>(
+                        sql "select * from RadicalValues where RadicalId in @Ids",
+                        param
+                    )
+
+                let radicalValuesByRadicalId = radicalValues |> Seq.groupBy _.RadicalId |> Map.ofSeq
+
+                let! radicalMeanings =
+                    ctx.QueryAsync<Tables.RadicalMeaning>(
+                        sql "select * from RadicalMeanings where RadicalId in @Ids",
+                        param
+                    )
+
+                let radicalMeaningsByRadicalId =
+                    radicalMeanings |> Seq.groupBy _.RadicalId |> Map.ofSeq
+
+                let! kanjiCounts =
+                    ctx.QueryAsync<RadicalKanjiCount>(
+                        sql
+                            "select RadicalId, count(*) as Count from Characters_Radicals where RadicalId in @Ids group by RadicalId",
+                        param
+                    )
+
+                let kanjiCountsByRadicalId =
+                    kanjiCounts |> Seq.map (fun x -> int x.RadicalId, int x.Count) |> Map.ofSeq
+
+                let! radicalNames =
+                    ctx.QueryAsync<RadicalNameValue>(
+                        sql
+                            """
+                        select rv.RadicalId, rn.Value
+                        from RadicalValues as rv
+                        join Characters as c on c.Value = rv.Value and c.IsRadical = 1
+                        join RadicalNames as rn on rn.CharacterId = c.Id
+                        where rv.RadicalId in @Ids
+                        """,
+                        param
+                    )
+
+                let radicalNamesByRadicalId =
+                    radicalNames
+                    |> Seq.groupBy _.RadicalId
+                    |> Seq.map (fun (rid, items) -> rid, (items |> Seq.map _.Value |> Seq.distinct |> Seq.toList))
+                    |> Map.ofSeq
+
+                return
+                    ids
+                    |> Seq.map (fun id ->
+                        let radical = radicalsById[id]
+
+                        {
+                            Id = id
+                            Number = radical.Number
+                            StrokeCount = radical.StrokeCount
+                            Values =
+                                radicalValuesByRadicalId
+                                |> Map.tryFind id
+                                |> Option.defaultValue []
+                                |> Seq.map _.Value
+                                |> Seq.toList
+                            Meanings =
+                                radicalMeaningsByRadicalId
+                                |> Map.tryFind id
+                                |> Option.defaultValue []
+                                |> Seq.map _.Value
+                                |> Seq.toList
+                            Names = radicalNamesByRadicalId |> Map.tryFind id |> Option.defaultValue []
+                            KanjiCount = kanjiCountsByRadicalId |> Map.tryFind id |> Option.defaultValue 0
+                        })
+        }
+
+    let getRadicalsAsync (query: GetRadicalQuery) (ctx: KensakuConnection) =
+        task {
+            let! ids = getRadicalIdsAsync query ctx
+            return! getRadicalsByIdsAsync ids ctx
+        }
+
+    let getRadicalLiteralsAsync (radicals: Rune list) (ctx: KensakuConnection) =
+        task {
+            let! ids =
+                ctx.QueryAsync<int>(
+                    sql "select distinct RadicalId from RadicalValues where Value in @Radicals",
+                    {| Radicals = List.map string radicals |}
+                )
+
+            return! getRadicalsByIdsAsync ids ctx
+        }
